a9a03b41859e1e50712c8767204584c0407e05af
Dias Utsman <utsmand91@gmail.com>
2024-07-31 13:18:32 +0700

    Add Web socket so it can both handle the share screen and the video chat

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var http = require('http');
 var https = require('https');
 var socketIO = require('socket.io');
 const port = process.env.PORT || 3030;
+const socket = require("websocket").server;
 
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
@@ -69,4 +70,99 @@ io.sockets.on('connection', function(socket) {
   });
 
 });
+
+
+const users = []
+
+const Types = {
+    SignIn: "SignIn",
+    StartStreaming: "StartStreaming",
+    UserFoundSuccessfully: "UserFoundSuccessfully",
+    Offer: "Offer",
+    Answer: "Answer",
+    IceCandidates: "IceCandidates",
+    EndCall: "EndCall",
+}
+const webSocket = new socket({httpServer: app})
+
+webSocket.on('request', (req) => {
+    const connection = req.accept();
+
+    connection.on('message', (message) => {
+        try {
+            const data = JSON.parse(message.utf8Data);
+            const currentUser = findUser(data.username)
+            const userToReceive = findUser(data.target)
+            console.log(data)
+
+            switch (data.type) {
+                case Types.SignIn:
+                    if (currentUser) {
+                        return
+                    }
+
+                    users.push({username: data.username, conn: connection, password: data.data})
+                    break
+                case Types.StartStreaming :
+                    if (userToReceive) {
+                            sendToConnection(userToReceive.conn, {
+                                type: Types.StartStreaming,
+                                username: currentUser.username,
+                                target: userToReceive.username
+                            })
+                    }
+                    break
+                case Types.Offer :
+                    if (userToReceive) {
+                        sendToConnection(userToReceive.conn, {
+                            type: Types.Offer, username: data.username, data: data.data
+                        })
+                    }
+                    break
+                case Types.Answer :
+                    if (userToReceive) {
+                        sendToConnection(userToReceive.conn, {
+                            type: Types.Answer, username: data.username, data: data.data
+                        })
+                    }
+                    break
+                case Types.IceCandidates:
+                    if (userToReceive) {
+                        sendToConnection(userToReceive.conn, {
+                            type: Types.IceCandidates, username: data.username, data: data.data
+                        })
+                    }
+                    break
+                case Types.EndCall:
+                    if (userToReceive) {
+                        sendToConnection(userToReceive.conn, {
+                            type: Types.EndCall, username: data.username
+                        })
+                    }
+                    break
+            }
+        } catch (e) {
+            console.log(e.message)
+        }
+
+    });
+    connection.on('close', () => {
+        users.forEach(user => {
+            if (user.conn === connection) {
+                users.splice(users.indexOf(user), 1)
+            }
+        })
+    })
+});
+
+
+const sendToConnection = (connection, message) => {
+    connection.send(JSON.stringify(message))
+}
+
+const findUser = username => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username) return users[i]
+    }
+}
 
